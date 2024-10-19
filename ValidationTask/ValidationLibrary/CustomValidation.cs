f8c2ec404909f24e2d88f64e8177e22f03ab1135
MiryusifMajidov <mecidovyusif079@gmail.com>
2024-10-20 01:14:35 +0400

    validation add

---
+++ ValidationTask/ValidationLibrary/CustomValidation.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationLibrary
+{
+    public class CustomValidator
+    {
+        public bool wordLength(string word)
+        {
+
+            if (word.Length < 2) return false;
+
+            else return true;
+
+        }
+        public bool age(int age)
+        {
+            if (age > 0) return true;
+
+            else return false;
+        }
+
+        public bool birthday(int birthday)
+        {
+            if (birthday < 1970) return true;
+
+            else return false;
+        }
+
+        public bool ValidatePassword(string password)
+        {
+
+            int checkNum = 0;
+            int checkUpper = 0;
+            int checkLower = 0;
+            int checkSpecial = 0;
+
+            if (password.Length < 8) return false;
+
+            foreach (char c in password)
+            {
+
+                if (c >= '0' && c <= '9')
+                {
+                    checkNum++;
+                }
+                if (c >= 'A' && c <= 'Z')
+                {
+                    checkUpper++;
+                }
+                if (c >= 'a' && c <= 'z')
+                {
+                    checkLower++;
+                }
+
+                if ((c >= 33 && c <= 47) ||  // ! " # $ % & ' ( ) * + , - . /
+                (c >= 58 && c <= 64) ||  // : ; < = > ? @
+                (c >= 91 && c <= 96) ||  // [ \ ] ^ _ `
+                (c >= 123 && c <= 126))  // { | } ~
+                {
+                    checkSpecial++;
+                }
+            }
+
+
+
+            if (checkNum == 0 || checkUpper == 0 || checkLower == 0 || checkSpecial == 0) return false;
+
+            return true;
+
+
+        }
+    }
+}
---
+++ ValidationTask/ValidationTask/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using ValidationLibrary;
+
+namespace ValidationTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CustomValidator sinifindən obyekt yaradılır
+            CustomValidator validator = new CustomValidator();
+
+            // Parolu yoxlayırıq
+            Console.Write("Istifadəçi adınızı daxil edin ");
+            string username = Console.ReadLine();
+
+            if (!validator.wordLength(username))
+            {
+                Console.WriteLine("username 2 herifden daha cox olmalidir");
+                return;
+            }
+
+            Console.Write("adınızı daxil edin ");
+            string name = Console.ReadLine();
+            
+
+            if (!validator.wordLength(name))
+            {
+                Console.WriteLine("name 2 herifden daha cox olmalidir");
+                return;
+            }
+
+
+            Console.Write("Soyadınızı daxil edin ");
+            string Surname = Console.ReadLine();
+
+            if (!validator.wordLength(Surname))
+            {
+                Console.WriteLine("name 2 herifden daha cox olmalidir");
+                return;
+            }
+
+            Console.Write("Yasiniz daxil edin ");
+            string ageInput = Console.ReadLine();
+            
+            int age = int.Parse(ageInput);
+            
+
+            if (!validator.age(age))
+            {
+                Console.WriteLine("Yasiniz 0 dan boyuk olmalidir");
+                return;
+            }
+
+            Console.Write("Dogum tarixiniz daxil edin ");
+            string birthdayInput = Console.ReadLine();
+
+            int birthday = int.Parse(birthdayInput);
+
+            if (!validator.birthday(birthday))
+            {
+                Console.WriteLine("1970 den boyuk olmalidir");
+                return;
+            }
+
+            Console.Write("Passwordunuz daxil edin ");
+            string password = Console.ReadLine();
+
+            if (!validator.ValidatePassword(password))
+            {
+                Console.WriteLine("Parol minimum 8 simvol olmalı, böyük hərf, kiçik hərf və rəqəm ehtiva etməlidir.");
+                return;
+            }
+
+            Person person = new Person() 
+            {
+                Username = username,
+                Password = password,
+                Name = name,
+                SureName = Surname,
+                Age = age,
+                Birthday = birthday
+            };
+
+            Console.WriteLine($"{person.Name} { person.SureName} {person.Username} adi ile person cedveline elave olundu ");
+
+            
+            
+
+
+
+        }
+    }
+}
