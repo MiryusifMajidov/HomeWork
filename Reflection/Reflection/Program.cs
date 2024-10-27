f87b6666e7de46041736801b6915c24f78b37735
MiryusifMajidov <mecidovyusif079@gmail.com>
2024-10-27 04:07:28 +0400

    Reflection added

---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Type userType = typeof(User);
+
+           
+            object userInstance = Activator.CreateInstance(userType);
+
+            // Property-lərin Reflection ilə əldə edilməsi
+            PropertyInfo idProperty = userType.GetProperty("Id");
+            PropertyInfo nameProperty = userType.GetProperty("Name");
+            PropertyInfo surnameProperty = userType.GetProperty("Surname");
+            PropertyInfo ageProperty = userType.GetProperty("Age");
+
+            
+             idProperty.SetValue(userInstance, 1);  
+            nameProperty.SetValue(userInstance, "miri");
+            surnameProperty.SetValue(userInstance, "majidov");
+
+           
+            ageProperty.SetValue(null, 19);
+
+            
+            Console.WriteLine("Məlumatlar:");
+            Console.WriteLine($"Id: {idProperty.GetValue(userInstance)}");
+            Console.WriteLine($"Name: {nameProperty.GetValue(userInstance)}");
+            Console.WriteLine($"Surname: {surnameProperty.GetValue(userInstance)}");
+            Console.WriteLine($"Age: {ageProperty.GetValue(null)}");
+
+           
+            Console.WriteLine("GetFullName method:");
+            MethodInfo getFullNameMethod = userType.GetMethod("GetFullName");
+            getFullNameMethod.Invoke(userInstance, null);
+
+          
+            Console.WriteLine("ChangeAge method:");
+            MethodInfo changeAgeMethod = userType.GetMethod("ChangeAge");
+            changeAgeMethod.Invoke(null, new object[] { 30 });
+
+           
+            Console.WriteLine($"New Age: {ageProperty.GetValue(null)}");
+        }
+    }
+}
---
+++ Reflection/Reflection/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class User
+    {
+       
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
+  
+        public string Surname { get; private set; }
+
+        public static int Age { get; private set; }
+        public User() { }
+        public User(int id, string name, string surname)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
+
+      
+        public void GetFullName()
+        {
+            Console.WriteLine($"{Name} {Surname}");
+        }
+
+       
+        public static void ChangeAge(int newAge)
+        {
+            Age = newAge;
+        }
+    }
+
+}
