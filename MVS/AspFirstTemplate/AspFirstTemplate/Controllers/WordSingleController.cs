cda37a470a2801e9711fe14ba0ecb1bc24f4c78a
MiryusifMajidov <mecidovyusif079@gmail.com>
2024-11-21 01:45:27 +0400

    mvs first template

---
+++ MVS/AspFirstTemplate/AspFirstTemplate/Controllers/WordSingleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspFirstTemplate.Controllers
+{
+    public class WordSingleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVS/AspFirstTemplate/AspFirstTemplate/Controllers/WorkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspFirstTemplate.Controllers
+{
+    public class WorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
